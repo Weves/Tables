76c657edc0ac5b5bf9c64716c2bbf7ffd2e6178a
Sushanth <sushanthmukkamalla@gmail.com>
2017-11-19 15:33:49 -0800

    Created all the interfaces.

--- app/src/main/java/com/msushanth/tablesapp/Interfaces/Profile/ProfileInterface.java
+++ app/src/main/java/com/msushanth/tablesapp/Interfaces/Profile/ProfileInterface.java
@@ -4,5 +4,5 @@ package com.msushanth.tablesapp.Interfaces.Profile;
  * Created by Sushanth on 11/19/17.
  */
 
-public interface ProfileInterface {
+public interface ProfileInterface extends PersonalProfileInterface, EditPersonalProfileInterface, SetStatusInterface {
 }
