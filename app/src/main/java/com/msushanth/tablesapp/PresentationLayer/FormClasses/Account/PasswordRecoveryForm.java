2096c240b93d1784e8c9979396ef23b136a8a52e
Sushanth <sushanthmukkamalla@gmail.com>
2017-11-25 00:53:37 -0800

    Edited layout of search tab

--- app/src/main/java/com/msushanth/tablesapp/PresentationLayer/FormClasses/Account/PasswordRecoveryForm.java
+++ app/src/main/java/com/msushanth/tablesapp/PresentationLayer/FormClasses/Account/PasswordRecoveryForm.java
@@ -62,7 +62,7 @@ public class PasswordRecoveryForm extends AppCompatActivity {
                 return;
             }
         }
-        
+
         passwordRecovery(email, getApplicationContext());
     }
 }
