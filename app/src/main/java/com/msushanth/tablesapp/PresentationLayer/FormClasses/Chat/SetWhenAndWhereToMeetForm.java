f8e0018eec00374556f692674e365a2c37d312e9
Xuan Zhang <xuz111@ucsd.edu>
2017-11-26 13:58:51 -0800

    setwhenandwhereinterface

--- app/src/main/java/com/msushanth/tablesapp/PresentationLayer/FormClasses/Chat/SetWhenAndWhereToMeetForm.java
+++ app/src/main/java/com/msushanth/tablesapp/PresentationLayer/FormClasses/Chat/SetWhenAndWhereToMeetForm.java
@@ -1,12 +1,16 @@
 package com.msushanth.tablesapp.PresentationLayer.FormClasses.Chat;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
 import com.msushanth.tablesapp.Interfaces.Chat.SetWhenAndWhereToMeetInterface;
 import com.msushanth.tablesapp.PresentationLayer.ActionClasses.Chat.SetWhenAndWheretoMeetAction;
+import com.msushanth.tablesapp.R;
+import com.msushanth.tablesapp.User;
 
 /**
  * Created by Sushanth on 11/9/17.
@@ -20,12 +24,21 @@ public class SetWhenAndWhereToMeetForm extends AppCompatActivity implements SetW
     String time;
     String Date;
     String meetingLocation;
-    /*@Override
+    User user;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(//TODO);
+        setContentView(R.layout.room_time_location);
+        meetingTimePicker = (TimePicker) findViewById(R.id.timePicker);
+        meetingDatePicker = (DatePicker) findViewById(R.id.datePicker);
+        meetingLocationText = (EditText) findViewById(R.id.LocationInputText);
 
-    }*/
+
+
+    }
+    public void submitButtonClicked(View view) {
+
+    }
     @Override
     public void setMeetingDate(String meetingDate){
         SetWhenAndWheretoMeetAction act = new SetWhenAndWheretoMeetAction();
